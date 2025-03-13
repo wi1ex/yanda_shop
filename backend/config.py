979a873199d31eea2099d09504de9c0b9888efe1
wi1ex <aleksanyan.artem@gmail.com>
2025-03-14 01:55:16 +0300

    4 create project with Vue.js and Aiogram

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your-telegram-bot-token")
-ADMIN_ID = os.getenv("ADMIN_ID", "your-admin-id")
+BOT_TOKEN = os.environ['BOT_TOKEN']
+ADMIN_ID = os.environ['ADMIN_ID']
