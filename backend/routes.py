4c93c4258acf69ab611bfbf27f87b4fa76a0ce5c
wi1ex <aleksanyan.artem@gmail.com>
2025-03-18 01:28:21 +0300

    45 create default project

--- backend/routes.py
+++ backend/routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, jsonify, request
 
-api = Blueprint("api", __name__, url_prefix="/api")
+api = Blueprint("api", __name__)
 
-@api.route("/")
+@api.route("/api")
 def home():
     return jsonify({"message": "Приложение работает!"})
 
