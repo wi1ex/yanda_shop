1cff4ca90f854ca6d7e39fa5236fecc72e1daf21
wi1ex <aleksanyan.artem@gmail.com>
2025-03-18 03:02:58 +0300

    45 create default project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: 8080
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html')
-    }
   }
 });
