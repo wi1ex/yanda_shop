63a25ef4f2b799481787e59b790eb411cb3ebe34
wilex <artpc@ya.ru>
2025-03-18 23:31:48 +0300

    53 create default project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
   root: "/frontend",
   publicDir: "public",
-  base: "/",
   build: {
     outDir: "dist",
-    manifest: true,
     rollupOptions: {
       input: "/frontend/public/index.html"
     }
