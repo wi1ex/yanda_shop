f8a76059cc4696659117e3c89ef15b6fcbdd6987
wi1ex <aleksanyan.artem@gmail.com>
2025-03-19 00:55:45 +0300

    57 create default project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
+  plugins: [vue()],
   root: "/frontend",
   publicDir: "public",
   resolve: {
     alias: {
-      '@': '/frontend/src'
+      '@': path.resolve(__dirname, 'src')
     }
   },
   build: {
