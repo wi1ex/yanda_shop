6c671ed9f01c44d6126cbe2cc1275ba5133476c9
wi1ex <aleksanyan.artem@gmail.com>
2025-03-18 15:16:14 +0300

    47 create default project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  root: path.resolve(__dirname, 'src'),  // Указываем корнем папку src
+  root: __dirname,  // Указываем корнем папку src
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     rollupOptions: {
       input: path.resolve(__dirname, 'src/index.html')  // Новый путь
     }
