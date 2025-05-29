8de4235558dedb3c275e7acb70744f55e3c4ad65
wilex <artpc@ya.ru>
2025-05-29 10:54:47 +0300

    81 add Shoe, Clothing, Accessory tables in db

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,6 @@ from models import db
 from routes import register_routes
 import logging
 import sys
-# from sqlalchemy.exc import ProgrammingError
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = (f"postgresql://{os.getenv('DB_USER')}:"
@@ -17,13 +16,6 @@ db.init_app(app)
 migrate = Migrate(app, db)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 register_routes(app)
-
-# Создаем таблицы при старте, если их нет
-# with app.app_context():
-#     try:
-#         db.create_all()
-#     except ProgrammingError:
-#         pass
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
