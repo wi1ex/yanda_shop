4ad9791e53fb02d9eafb19a7324554e3577c813f
wilex <artpc@ya.ru>
2025-06-03 17:51:20 +0300

    102 backend optimization

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,12 @@
-import os
-from flask import Flask
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import db
+from flask import Flask
 from routes import register_routes
+from models import db
+import extensions
 import logging
 import sys
+import os
 
 app = Flask(__name__)
 
@@ -13,8 +14,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (f"postgresql://{os.getenv('DB_USER')}:"
                                          f"{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:"
                                          f"{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 
+db.init_app(app)
 migrate = Migrate(app, db)
 
 CORS(app, resources={r"/api/*": {"origins": "https://shop.yanda.twc1.net"}})
